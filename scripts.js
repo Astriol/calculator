3f137fb96894d1b525e8b9128c18990704b1a67a
Paige Mundy <paige.mundy@student.swic.edu>
2019-10-02 20:27:59 -0500

    week 6 calling functions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,37 @@
-// Enter a description of this program here.
+// Simple arithmetic calculator app
 
 /**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
+ 3. Log the value of the select and the inputs.
  */
+
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function remainder(a, b) {
+  return a % b;
+}
+
+const form = document.querySelector("form");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("#num-2");
+const select = document.querySelector("select");
+
+// Use an anonymous callback function as the 2nd argument.
+form.addEventListener("submit", function(event) {
+  //Prevent the default brwser behavioe -- DON'T submit
+  event.preventDefault();
+  console.log(num1.value, num2.value, select.value);
+});
